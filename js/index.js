d562491e7322955bf53bca49a5d026031c3ec2ae
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2023-11-21 23:16:46 +0100

    Save in localStorage
    Added code to remember already opened advent calendar boxes.

--- js/index.js
+++ js/index.js
@@ -3,10 +3,27 @@ import adventBoxes from "./data/api.js";
 //Make sure there is a div/container to push to in index
 const adventCalendarContainer = document.querySelector(".advent-calendar");
 
+const savedBoxes = getExistingBoxes();
+
 adventBoxes.forEach(adventBox => {
+
+  let cssClass = "show";
+
+  const doesObjectExist = savedBoxes.find(function (opened) {
+    console.log(opened);
+
+    return parseInt(opened.id) === adventBox.id;
+  });
+
+  console.log(doesObjectExist);
+
+  if (doesObjectExist) {
+    cssClass = "transparent";
+  }
+
   adventCalendarContainer.innerHTML += `
-  <div class="advent-box" style="background-image:url('${adventBox.boxCover}');" data-id="${adventBox.id}" data-name="${adventBox.title}">
-    <h2 class="show">${adventBox.box}</h2>
+  <div class="advent-box" style="background-image:url('${adventBox.boxCover}');" data-id="${adventBox.id}" data-title="${adventBox.title}">
+    <h2 class="${cssClass}">${adventBox.box}</h2>
   </div>`;
 });
 
@@ -22,4 +39,47 @@ function handleClick() {
   // console.log(event);
   event.target.classList.toggle("transparent");
   event.target.classList.toggle("show");
+
+  const id = this.dataset.id;
+  const title = this.dataset.title;
+
+  // console.log("id", id);
+  // console.log("title", title);
+
+  const currentOpenedBoxes = getExistingBoxes();
+  // console.log(currentOpenedBoxes);
+
+  const productExists = currentOpenedBoxes.find(function (opened) {
+    return opened.id === id;
+  });
+
+  if (!productExists) {
+    const product = { id: id, title: title };
+    currentOpenedBoxes.push(product);
+    saveOpenedBoxes(currentOpenedBoxes);
+  }
+  else {
+    const newOpenedBoxes = currentOpenedBoxes.filter(opened => opened.id !== id);
+    saveOpenedBoxes(newOpenedBoxes);
+  }
+
+
+}
+
+function getExistingBoxes() {
+  const openedBox = localStorage.getItem("openedBoxes");
+
+  // console.log(openedBox);
+
+  if (!openedBox) {
+    return [];
+  }
+  else {
+    return JSON.parse(openedBox);
+  }
+
+}
+
+function saveOpenedBoxes(openedBox) {
+  localStorage.setItem("openedBoxes", JSON.stringify(openedBox));
 }
