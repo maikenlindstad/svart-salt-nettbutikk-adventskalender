132af1abb73f05571d8cd8d1e84a176c89944495
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2023-11-21 21:04:28 +0100

    Various additions
    Added design images, placeholder for the advent boxes, styled project, api and other code. + Updated README.md

---
+++ js/index.js
@@ -0,0 +1,25 @@
+import adventBoxes from "./data/api.js";
+
+//Make sure there is a div/container to push to in index
+const adventCalendarContainer = document.querySelector(".advent-calendar");
+
+adventBoxes.forEach(adventBox => {
+  adventCalendarContainer.innerHTML += `
+  <div class="advent-box" style="background-image:url('${adventBox.boxCover}');">
+    <h2 class="show">${adventBox.box}</h2>
+  </div>`;
+});
+
+const flipButton = document.querySelectorAll(".advent-box");
+
+// console.log(flipButton);
+
+flipButton.forEach((button) => {
+  button.addEventListener("click", handleClick);
+})
+
+function handleClick() {
+  // console.log(event);
+  event.target.classList.toggle("transparent");
+  event.target.classList.toggle("show");
+}
